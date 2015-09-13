673b2cac64e988730c33dc605088cb0973ce6e43
Elaine Huynh <elainehuynh@dhcp-rhodes-2487.redrover.cornell.edu>
2015-09-13 15:08:18 -0400

    Initial Commit

---
+++ Assignment2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Assignment2
+//
+//  Created by Elaine Huynh on 9/13/15.
+//  Copyright (c) 2015 Elaine Huynh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Assignment2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Assignment2
+//
+//  Created by Elaine Huynh on 9/13/15.
+//  Copyright (c) 2015 Elaine Huynh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
